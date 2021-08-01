641069322a1770f2cbfc9e20cd61d5028f314be3
Sandep <connectme.sjaina@outlook.com>
2021-08-01 19:46:22 +0530

    asfsgsdgdsgds

--- Linux/Roboshop/frontend.sh
+++ Linux/Roboshop/frontend.sh
@@ -3,12 +3,20 @@
 log=/tmp/roboshop.log
 rm -f $log
 echo -n -e "Installing Nginix Server\t\t ..."
-yum install nginx -y &>> $log
+yum install nginx -y &>>$log
 if($? -eq 0); then echo -e "\e[32m Done\e[0m"
 else
   echo Fail
   fi
 echo "Enabling nginx service\t"
-systemctl enable nginx &>> $log
+systemctl enable nginx &>>$log
+if($? -eq 0); then echo -e "\e[32m Done\e[0m"
+else
+  echo Fail
+  fi
 echo "Starting ngnix server\t"
-systemctl start nginx &>> $log
+systemctl start nginx &>>$log
+if($? -eq 0); then echo -e "\e[32m Done\e[0m"
+else
+  echo Fail
+  fi
