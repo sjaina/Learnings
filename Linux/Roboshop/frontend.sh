df0dfa3586244d87b7062bcb969bfa28341a431d
Sandep <connectme.sjaina@outlook.com>
2021-08-01 21:00:54 +0530

    ndkgdnskgnklngslkng

--- Linux/Roboshop/frontend.sh
+++ Linux/Roboshop/frontend.sh
@@ -6,20 +6,20 @@ yum install nginx -y &>>$log
 status_check $?
 
 print "Downloading Frontend code"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>log
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$log
 status_check $?
 
 
 print "Removing old docs"
-cd /usr/share/nginx/html &>>log && rm -rf * &>>log
+cd /usr/share/nginx/html &>>log && rm -rf * &>>$log
 status_check $?
 
 print "Unzipping the code"
-unzip /tmp/frontend.zip &>>log && mv frontend-main/* . &>>log && mv static/* &>>log && rm -rf frontend-master static &>>log
+unzip /tmp/frontend.zip &>>$log && mv frontend-main/* . &>>$log && mv static/* &>>$log && rm -rf frontend-master static &>>$log
 status_check $?
 
 print "moving roboshop config"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>log
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$log
 status_check $?
 
 
