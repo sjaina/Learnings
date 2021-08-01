e8e0a60e129da33d9312e5cff2bccc073b3e8197
Sandep <connectme.sjaina@outlook.com>
2021-08-01 19:47:53 +0530

    awfaf

--- Linux/Roboshop/frontend.sh
+++ Linux/Roboshop/frontend.sh
@@ -4,19 +4,19 @@ log=/tmp/roboshop.log
 rm -f $log
 echo -n -e "Installing Nginix Server\t\t ..."
 yum install nginx -y &>>$log
-if($? -eq 0); then echo -e "\e[32m Done\e[0m"
+if[$? -eq 0]; then echo -e "\e[32m Done\e[0m"
 else
   echo Fail
   fi
 echo "Enabling nginx service\t"
 systemctl enable nginx &>>$log
-if($? -eq 0); then echo -e "\e[32m Done\e[0m"
+if[$? -eq 0]; then echo -e "\e[32m Done\e[0m"
 else
   echo Fail
   fi
 echo "Starting ngnix server\t"
 systemctl start nginx &>>$log
-if($? -eq 0); then echo -e "\e[32m Done\e[0m"
+if[$? -eq 0]; then echo -e "\e[32m Done\e[0m"
 else
   echo Fail
   fi
