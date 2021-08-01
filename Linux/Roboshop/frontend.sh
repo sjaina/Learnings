3bfb03ce35a58625fb3ac44b5ea0d979611d4ef5
Sandep <connectme.sjaina@outlook.com>
2021-08-01 19:51:56 +0530

    3535asf

--- Linux/Roboshop/frontend.sh
+++ Linux/Roboshop/frontend.sh
@@ -9,14 +9,14 @@ if [ $? -eq 0 ]; then
 else
   echo Fail
   fi
-echo "Enabling nginx service\t"
+echo -n -e "Enabling nginx service\t"
 systemctl enable nginx &>>$log
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Done\e[0m"
 else
   echo Fail
   fi
-echo "Starting ngnix server\t"
+echo -n -e "Starting ngnix server\t"
 systemctl start nginx &>>$log
 if [ $? -eq 0 ]; then
   echo -e "\e[32m Done\e[0m"
