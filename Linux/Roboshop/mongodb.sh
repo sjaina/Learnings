7284b79ba24af913c562ed2b56379cb6fe2722a6
Sandep <connectme.sjaina@outlook.com>
2021-08-01 23:05:28 +0530

    ksnlgknslkgsl

--- Linux/Roboshop/mongodb.sh
+++ Linux/Roboshop/mongodb.sh
@@ -10,19 +10,25 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo  &>>$log
 status_check $?
 
-# yum install -y mongodb-org
-# systemctl enable mongod
-# systemctl start mongod
+print "Installing Mongodb"
+ yum install -y mongodb-org  &>>$log
+ status_check $?
 
-#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
-#Config file: /etc/mongod.conf
+print "Adding 0.0.0.0 in config file"
 
-# systemctl restart mongod
+sed -i -e '/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>$log
+status_check $?
 
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+print "Enabling and starting MongoDB service"
+systemctl enable mongod &>>$log && systemctl start mongod &>>$log
+status_check $?
 
-# cd /tmp
-# unzip mongodb.zip
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js
+Print "Downloading schema \t"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$log
+status_check $?
+
+Print "unzip and loading the schema"
+cd /tmp &>>$log && unzip mongodb.zip &>>$log && cd mongodb-main  &>>$log
+status_check $?
+
+ #mongo < catalogue.js && mongo < users.js
