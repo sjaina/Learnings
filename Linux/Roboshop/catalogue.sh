f3a9f1db898902de3bffebe43c08cf22aef5d4a2
Sandep <connectme.sjaina@outlook.com>
2021-08-02 11:19:15 +0530

    ksjksndgns

--- Linux/Roboshop/catalogue.sh
+++ Linux/Roboshop/catalogue.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 source common.sh
+
+print "Installing NodeJS"
+yum install nodejs make gcc-c++ -y  &>>$log
+status_check $?
+
+print "Add roboshop user"
+useradd roboshop &>>$log
+status_check $?
+
+#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+#$ cd /home/roboshop
+#$ unzip /tmp/catalogue.zip
+#$ mv catalogue-main catalogue
+#$ cd /home/roboshop/catalogue
+#$ npm install
+
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
