f45abf74876b79ee5dd7a578124dd03e910c2cab
Sandep <connectme.sjaina@outlook.com>
2021-08-02 10:40:32 +0530

    kslndknsklgnksd

--- Linux/Roboshop/mongodb.sh
+++ Linux/Roboshop/mongodb.sh
@@ -7,7 +7,7 @@ name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo &>>$log
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 status_check $?
 
 print "Installing Mongodb"
