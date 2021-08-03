99b72be71b6d61daa44c740bc41cd190757667db
Sandep <connectme.sjaina@outlook.com>
2021-08-03 22:20:39 +0530

    laksfkll

--- Linux/Roboshop/frontend.sh
+++ Linux/Roboshop/frontend.sh
@@ -17,20 +17,5 @@ status_check $?
 print "Unzipping the code"
 unzip /tmp/frontend.zip &>>$log && mv frontend-main/* . &>>$log && mv static/* . &>>$log && rm -rf frontend-master static &>>$log
 status_check $?
-
-print "moving roboshop config"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$log
-status_check $?
-
-print "Update config file"
-sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/'  -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
-status_check $?
-
-# systemctl restart nginx
-print "Enabling nginx service\t"
-systemctl enable nginx &>>$log
-status_check $?
-
-print "Starting ngnix server\t"
-systemctl restart nginx &>>$log
+nginx &>>$log
 status_check $?
